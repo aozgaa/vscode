b6ab82089ee0cb3ce381a6c39526ad6717e2fd6d
Arthur Ozga <aozgaa-ms@outlook.com>
2017-01-04 14:00:25 -0800

    fix typo

--- src/vs/platform/instantiation/common/instantiation.ts
+++ src/vs/platform/instantiation/common/instantiation.ts
@@ -181,7 +181,7 @@ function storeServiceDependency(id: Function, target: Function, index: number, o
 }
 
 /**
- * A *only* valid way to create a {{ServiceIdentifier}}.
+ * The *only* valid way to create a {{ServiceIdentifier}}.
  */
 export function createDecorator<T>(serviceId: string): { (...args: any[]): void; type: T; } {
 
